551b289aeaa243e1e1c3af6ec19f2e8638a17a4c
lokeshbhardwaj20 <128887794+lokeshbhardwaj20@users.noreply.github.com>
2023-07-04 10:44:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+// Get the button element
+const showHeadingBtn = document.getElementById('submitHandle');
+
+// Add a click event listener to the button
+showHeadingBtn.addEventListener('click', () => {
+  // Create a new heading element
+  const heading = document.createElement('h5');
+  heading.textContent = 'Your request is submitted, Thankyou so much😃!!';  
+  heading.style.color = 'green';
+  
+  // Get the container element where the heading will be displayed
+  const container = document.getElementById('greenHeadingContainer');
+  
+  // Append the heading to the container
+  container.appendChild(heading);
+    // Set the duration (in milliseconds) for the heading to be displayed
+    const duration = 2000;
+
+    // Remove the heading after the specified duration
+    setTimeout(() => {
+      container.removeChild(heading);
+    }, duration);
+});
+
